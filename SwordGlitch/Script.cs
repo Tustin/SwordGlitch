1d8595cf79bf33920fb0675381887c110607b138
Josh <NGUTustin@gmail.com>
2017-12-11 19:00:21 -0600

    initial commit

---
+++ SwordGlitch/Script.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PS4MacroAPI;
+
+namespace SwordGlitch
+{
+    public class Script : ScriptBase
+    {
+		public Script()
+		{
+			Config.Name = "Sword Glitch Macro";
+			Config.LoopDelay = 1;
+		}
+
+		public override void Start()
+		{
+			base.Start();
+		}
+
+		public override void Update()
+		{
+			Press(new DualShockState() { Square = true }, 250);
+		}
+	}
+}
